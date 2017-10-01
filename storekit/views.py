4a648e428e11ae37aeb78f987379ee0c678410f5
nnsnodnb <ahr63_gej@me.com>
2017-10-01 18:08:17 +0900

    Add recipt view

--- storekit/views.py
+++ storekit/views.py
@@ -1,2 +1,9 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+
+
+@require_POST
+@csrf_exempt
+def receipt(request):
+    return HttpResponse()
