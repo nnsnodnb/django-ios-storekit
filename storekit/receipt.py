e2bc30c4a25d16fccb5f4473366e564219203e2c
nnsnodnb <ahr63_gej@me.com>
2017-10-10 22:31:27 +0900

    Update receipt.py

--- storekit/receipt.py
+++ storekit/receipt.py
@@ -1,30 +1,22 @@
 from django.conf import settings
-
-import json
-import requests
+from pyinapp import AppStoreValidator, InAppValidationError
 
 
-def subscribe_receipt(receipt, sandbox):
-    url = 'https://sandbox.itunes.apple.com/verifyReceipt' if sandbox else 'https://buy.itunes.apple.com/verifyReceipt'
-    response = requests.post(
-        url=url,
-        data=json.dumps({
-            'receipt-data': receipt,
-            'password': settings.STOREKIT_PURCHASED_SECRET if settings.STOREKIT_PURCHASED_SECRET != '' else ''
-        })
-    ).json()
+validator = AppStoreValidator(settings.STOREKIT_APP_BUNDLE_ID)
 
-    if response['status'] != 0 and response['status'] != 21006:
-        return None
 
-    if response['latest_receipt_info'] is None:
-        return None
+def subscribe_receipt(receipt):
+    try:
+        purchases = validator.validate(receipt)
+        process_purchases(purchases)
+    except InAppValidationError:
+        """ handle validation error """
 
-    expires_date_ms = 0
-    for info in response['latest_receipt_info']:
-        if info['product_id'] is None or info['expires_date_ms'] is None:
-            continue
-        expires_date_ms = expires_date_ms \
-            if expires_date_ms > int(info['expires_date_ms']) else int(info['expires_date_ms'])
 
-    return {'expire': expires_date_ms / 1000}
+def process_purchases(purchases):
+    process(*purchases) if isinstance(purchases, list) else process(purchases)
+
+
+def process(*purchases):
+    for p in purchases:
+        """ for instance, save p to db and add a player some coins for it """
