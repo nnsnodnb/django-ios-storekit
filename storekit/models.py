b52d765c4a771218e769c64d41e5cfb02157c5b7
nnsnodnb <ahr63_gej@me.com>
2017-12-10 16:07:47 +0900

    Update storekit.models' initializer

--- storekit/models.py
+++ storekit/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from pyinapp.purchase import Purchase as LibPurchase
 
 
 class InApp(models.Model):
@@ -14,11 +15,49 @@ class InApp(models.Model):
     original_purchase_date_pst = models.CharField(blank=False, default='', max_length=255)
     is_trial_period = models.BooleanField(blank=False, default=False)
 
+    def __init__(self, quantity: int, product_id: str, transaction_id: int, original_transaction_id: int,
+                 purchase_date: str, purchase_date_ms: int, purchase_date_pst: str, original_purchase_date: str,
+                 original_purchase_date_ms: int, original_purchase_date_pst: str, is_trial_period: bool):
+        super(InApp, self).__init__(quantity, product_id, transaction_id, original_transaction_id, purchase_date,
+                                    purchase_date_ms, purchase_date_pst, original_purchase_date,
+                                    original_purchase_date_ms, original_purchase_date_pst, is_trial_period)
+        self.quantity = quantity
+        self.product_id = product_id
+        self.transaction_id = transaction_id
+        self.original_transaction_id = original_transaction_id
+        self.purchase_date = purchase_date
+        self.purchase_date_ms = purchase_date_ms
+        self.purchase_date_pst = purchase_date_pst
+        self.original_purchase_date = original_purchase_date
+        self.original_purchase_date_ms = original_purchase_date_ms
+        self.original_purchase_date_pst = original_purchase_date_pst
+        self.is_trial_period = is_trial_period
+
     def __str__(self):
         return '{}: {}'.format(self.product_id, self.quantity)
 
     def __repr__(self):
         return '{} <ID: {}>: "{}"'.format(self.product_id, self.id, self.__class__.__name__)
+
+    @classmethod
+    def parser(cls, json, is_save: bool=True):
+        in_app = {
+            'quantity': int(json['quantity']),
+            'product_id': json['product_id'],
+            'transaction_id': int(json['transaction_id']),
+            'original_transaction_id': int(json['original_transaction_id']),
+            'purchase_date': json['purchase_date'],
+            'purchase_date_ms': int(json['purchase_date_ms']),
+            'purchase_date_pst': json['purchase_date_pst'],
+            'original_purchase_date': json['original_purchase_date'],
+            'original_purchase_date_ms': int(json['original_purchase_date_ms']),
+            'original_purchase_date_pst': json['original_purchase_date'],
+            'is_trial_period': bool(json['is_trial_period'])
+        }
+        obj = cls(**in_app)
+        if is_save:
+            obj.save()
+        return obj
 
 
 class Receipt(models.Model):
@@ -41,11 +80,49 @@ class Receipt(models.Model):
     original_application_version = models.CharField(blank=False, default='', max_length=255)
     in_app = models.ForeignKey(InApp, blank=True)
 
+    def __init__(self, receipt_type: str, adam_id: int, app_item_add: int, bundle_id: str,
+                 application_version: str, download_id: int, version_external_identifier: int,
+                 receipt_creation_date: str, receipt_creation_date_ms: int, receipt_creation_date_pst: str,
+                 request_date: str, request_date_ms: int, request_date_pst: str, original_purchase_date: str,
+                 original_purchase_date_ms: int, original_purchase_date_pst: str, original_application_version: str,
+                 in_app: InApp):
+        super(Receipt, self).__init__(receipt_type, adam_id, app_item_add, bundle_id, application_version, download_id,
+                                      version_external_identifier, receipt_creation_date, receipt_creation_date_ms,
+                                      receipt_creation_date_pst, request_date, request_date_ms, request_date_pst,
+                                      original_purchase_date, original_purchase_date_ms, original_purchase_date_pst,
+                                      original_application_version, in_app)
+
     def __str__(self):
         return '【{}】{}: {}'.format(self.receipt_type, self.bundle_id, self.application_version)
 
     def __repr__(self):
         return '<ID: {}>: "{}"'.format(self.id, self.__class__.__name__)
+
+    @classmethod
+    def parser(cls, json, is_save: bool=True):
+        receipt = {
+            'receipt_type': json['receipt_type'],
+            'adam_id': int(json['adam_id']),
+            'app_item_id': int(json['app_item_id']),
+            'bundle_id': json['bundle_id'],
+            'application_version': json['application_version'],
+            'download_id': int(json['download_id']),
+            'version_external_identifier': int(json['version_external_identifier']),
+            'receipt_creation_date': json['receipt_creation_date'],
+            'receipt_creation_date_ms': int(json['receipt_creation_date_ms']),
+            'receipt_creation_date_pst': json['rceipt_creation_date_pst'],
+            'request_date': json['request_date'],
+            'request_date_ms': int(json['request_date_ms']),
+            'request_date_pst': json['request_date_pst'],
+            'original_purchase_date': json['original_purchase_date'],
+            'original_purchase_date_ms': int(json['original_purchase_date_ms']),
+            'original_purchase_date_pst': json['original_purchase_date_pst'],
+            'in_app': InApp.parser(json['in_app'])
+        }
+        obj = cls(**receipt)
+        if is_save:
+            obj.save()
+        return obj
 
 
 class Response(models.Model):
@@ -53,22 +130,63 @@ class Response(models.Model):
     environment = models.CharField(blank=False, default='', max_length=255)
     receipt = models.ForeignKey(Receipt, blank=True)
 
+    def __init__(self, status: int, environment: str, receipt: Receipt):
+        super(Response, self).__init__(status, environment, receipt)
+        self.status = status
+        self.environment = environment
+        self.receipt = receipt
+
     def __str__(self):
         return 'Response: ' + self.environment
 
     def __repr__(self):
         return '<ID: {}>: "{}"'.format(self.id, self.__class__.__name__)
 
+    @classmethod
+    def parser(cls, json, is_save: bool=True):
+        response = {
+            'status': json['status'],
+            'environment': json['environment'],
+            'receipt': Receipt.parser(json['receipt'])
+        }
+        obj = cls(**response)
+        if is_save:
+            obj.save()
+        return obj
 
-class Purchase(models.Model):
+
+class Purchase(models.Model, LibPurchase):
     transaction_id = models.IntegerField(blank=True)
     product_id = models.IntegerField(blank=True)
     quantity = models.IntegerField(blank=False, default=0)
     purchased_at = models.CharField(blank=False, default='', max_length=255)
     response = models.ForeignKey(Response, blank=True)
 
+    def __init__(self, transaction_id: str, product_id: int, quantity: int, purchased_at: str, response: Response):
+        super(Purchase, self).__init__(transaction_id, product_id, quantity, purchased_at, response)
+        super(LibPurchase, self).__init__()
+        self.transaction_id = transaction_id
+        self.product_id = product_id
+        self.quantity = quantity
+        self.purchased_at = purchased_at
+        self.response = response
+
     def __str__(self):
         return 'Purchase: ' + str(self.transaction_id)
 
     def __repr__(self):
         return '{} <ID: {}>: "{}"'.format(self.product_id, self.id, self.__class__.__name__)
+
+    @classmethod
+    def parser(cls, json, is_save: bool=True):
+        purchase = {
+            'transaction_id': json['transaction_id'],
+            'product_id': json['product_id'],
+            'quantity': int(json['quantity']),
+            'purchased_at': json['purchased_at'],
+            'response': Response.parser(json['response'])
+        }
+        obj = cls(**purchase)
+        if is_save:
+            obj.save()
+        return obj
