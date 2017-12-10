0d9403eeac87fe2d52b482e7807024c3652083ca
nnsnodnb <ahr63_gej@me.com>
2017-12-10 16:22:49 +0900

    Update receipt.py

--- storekit/receipt.py
+++ storekit/receipt.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from pyinapp import AppStoreValidator, InAppValidationError
-from storekit.models import InApp, Receipt, Response, Purchase
+from storekit.models import Purchase
 import logging
 
 
@@ -16,8 +16,7 @@ def subscribe_receipt(receipt, sandbox):
                 receipt=receipt,
                 password=settings.STOREKIT_PURCHASED_SECRET
             )
-            _process_purchases(purchases)
-            return True
+            return _process(purchases)
 
         except InAppValidationError as e:
             logging.error(e)
@@ -28,11 +27,9 @@ def subscribe_receipt(receipt, sandbox):
         pass
 
 
-def _process_purchases(purchases):
-    _process(purchases) if isinstance(purchases, list) else _process(purchases)
-
-
-def _process(purchases):
-    for p in purchases:
-        purchase = Purchase.parser(p)
-        purchase.save()
+def _process(purchases) -> list:
+    return list(
+        map(
+            lambda purchase: Purchase.parser(purchase), purchases
+        )
+    )
