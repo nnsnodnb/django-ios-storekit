06e680e5b5b7762b2c94eb02d7baf1a4771ff0ea
nnsnodnb <ahr63_gej@me.com>
2017-12-23 07:55:11 +0900

    Add base test class for receipt.py

--- storekit/tests/test_receipt.py
+++ storekit/tests/test_receipt.py
@@ -2,5 +2,19 @@ from unittest import TestCase
 from storekit.receipt import normal_receipt, subscribe_receipt
 
 
-class ReceiptTest(TestCase):
-    pass
+class NormalReceiptTest(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+class SubscribeReceiptTest(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
