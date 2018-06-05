0246cf1f1691857769c32ac16fe317b49b7694e7
nnsnodnb <ahr63_gej@me.com>
2018-06-06 00:28:51 +0900

    Recreate demo project

--- demo/app/views.py
+++ demo/app/views.py
@@ -1,22 +1,29 @@
 from django.http.response import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from storekit.errors import AppValidationError
 from storekit.receipt import normal_receipt, subscribe_receipt
 
+import json
 
+
+@csrf_exempt
 def receive_normal_receipt(request):
     if request.method == 'POST':
         try:
-            _ = normal_receipt(receipt=request.POST['receipt_data'], sandbox=False)
+            body = json.loads(request.body.decode('utf-8'))
+            _ = normal_receipt(receipt=body['receipt-data'], sandbox=True)
         except AppValidationError or AttributeError:
             return HttpResponse(status=400)
 
         return HttpResponse(status=200)
 
 
+@csrf_exempt
 def receive_subscribe_receipt(request):
     if request.method == 'POST':
         try:
-            _ = subscribe_receipt(receipt=request.POST['receipt_data'], sandbox=False)
+            body = json.loads(request.body.decode('utf-8'))
+            _ = subscribe_receipt(receipt=body['receipt-data'], sandbox=True)
         except AppValidationError or AttributeError:
             return HttpResponse(status=400)
 
