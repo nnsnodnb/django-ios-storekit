fbdab22ffe6cb2c1f1e0e9d389d0f05b296b7d62
nnsnodnb <nnsnodnb@gmail.com>
2019-07-26 16:49:55 +0900

    Update resources

--- setup.py
+++ setup.py
@@ -1,22 +1,14 @@
 from setuptools import setup
 
 
-def read(filename):
-    import os
-    BASE_DIR = os.path.dirname(__file__)
-    filename = os.path.join(BASE_DIR, filename)
-    with open(filename, 'r') as f:
-        return f.read()
-
-
 def readlist(filename):
-    rows = read(filename).split('\n')
+    rows = open(filename).read().split('\n')
     rows = [x.strip() for x in rows if x.strip()]
     return list(rows)
 
 
 setup(
-    long_description=read('README.md'),
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     download_url='https://github.com/nnsnodnb/django-ios-storekit/tarball/master',
     include_package_data=True,
