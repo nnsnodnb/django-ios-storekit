35f8203810d91b34a0a1f9f5591bce90b2a87064
Yuya Oka <nnsnodnb@gmail.com>
2021-06-07 18:07:40 +0900

    Update for flake8

--- tests/test_errors.py
+++
@@ -1,8 +0,0 @@
-from storekit.errors import AppValidationError
-
-
-def test_app_validation_error():
-    error = AppValidationError(msg='')
-    error.response = {'status': 1}
-
-    assert error.response is not None
